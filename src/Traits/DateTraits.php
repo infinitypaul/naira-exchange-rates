b51be9fca4b9d54fd9957974549f8f0d3b839b1f
Paul Edward <infinitypaul@live.com>
2020-03-14 20:58:55 +0100

    Apply fixes from StyleCI (#1)

--- src/Traits/DateTraits.php
+++ src/Traits/DateTraits.php
@@ -1,58 +1,54 @@
 <?php
 
-
 namespace Infinitypaul\NairaExchangeRates\Traits;
 
 use Infinitypaul\NairaExchangeRates\Exceptions\Exceptions;
 
 trait DateTraits
 {
-    # Get the "from" date:
+    // Get the "from" date:
     public function getDateFrom()
     {
         return $this->dateFrom;
     }
 
-    # Get the "to" date:
+    // Get the "to" date:
     public function getDateTo()
     {
         return $this->dateTo;
     }
 
-    # Add a date-from:
-    public function addDateFrom( string $from )
+    // Add a date-from:
+    public function addDateFrom(string $from)
     {
-        if( $this->validateDateFormat($from) )
-        {
+        if ($this->validateDateFormat($from)) {
             $this->dateFrom = $from;
 
-            # Return object to preserve method-chaining:
+            // Return object to preserve method-chaining:
             return $this;
         }
 
         throw Exceptions::create('format.invalid_date');
     }
 
-    # Add a date-to:
-    public function addDateTo( string $to )
+    // Add a date-to:
+    public function addDateTo(string $to)
     {
-        if( $this->validateDateFormat($to) )
-        {
+        if ($this->validateDateFormat($to)) {
             $this->dateTo = $to;
 
-            # Return object to preserve method-chaining:
+            // Return object to preserve method-chaining:
             return $this;
         }
 
         throw Exceptions::create('format.invalid_date');
     }
 
-    # Validate a date is in the correct format:
-    private function validateDateFormat( string $date = null )
+    // Validate a date is in the correct format:
+    private function validateDateFormat(string $date = null)
     {
-        if( !is_null($date) )
-        {
-            return preg_match( $this->dateRegExp, $date);
+        if (! is_null($date)) {
+            return preg_match($this->dateRegExp, $date);
         }
 
         return false;
--- src/Traits/TypeTraits.php
+++ src/Traits/TypeTraits.php
@@ -1,63 +1,60 @@
 <?php
 
-
 namespace Infinitypaul\NairaExchangeRates\Traits;
-
 
 use Infinitypaul\NairaExchangeRates\Exceptions\Exceptions;
 
 trait TypeTraits
 {
-    # Set the base type:
-    public function setType( string $type )
+    // Set the base type:
+    public function setType(string $type)
     {
-        # Sanitize the code:
+        // Sanitize the code:
         $type = $this->sanitizeTypes($type);
 
         $this->verifyType($type);
 
         $this->type = $type;
 
-        # Return object to preserve method-chaining:
+        // Return object to preserve method-chaining:
         return $this;
     }
 
-    # Get the specified type:
+    // Get the specified type:
     public function getBaseType()
     {
         return (is_null($this->type)) ? 'cbn' : $this->type;
     }
 
-    # Sanitize types:
-    private function sanitizeTypes( string $code )
+    // Sanitize types:
+    private function sanitizeTypes(string $code)
     {
         return trim(
-            strtolower( $code )
+            strtolower($code)
         );
     }
 
     /**
-     * Verify Types
+     * Verify Types.
      * @param \Infinitypaul\NairaExchangeRates\Traits\string $code
      *
      * @throws \Infinitypaul\NairaExchangeRates\Exceptions\Exceptions
      */
-    private function verifyType(string $code )
+    private function verifyType(string $code)
     {
         $type = $this->sanitizeTypes($code);
 
-        # Is it a supported type?
-        if( ! $this->typeIsSupported($type) )
-        {
+        // Is it a supported type?
+        if (! $this->typeIsSupported($type)) {
             throw Exceptions::create('format.unsupported_type');
         }
     }
 
-    # Check if a type is in the supported range:
-    private function typeIsSupported( string $code )
+    // Check if a type is in the supported range:
+    private function typeIsSupported(string $code)
     {
         $type = $this->sanitizeTypes($code);
 
-        return in_array( $type, $this->_types );
+        return in_array($type, $this->_types);
     }
 }
