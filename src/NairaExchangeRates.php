da6ad8979e7a204f186d7f8a99910d2df5c790e6
Paul Edward <infinitypaul@live.com>
2020-03-14 19:58:48 +0000

    Apply fixes from StyleCI

--- src/NairaExchangeRates.php
+++ src/NairaExchangeRates.php
@@ -9,60 +9,57 @@ use Infinitypaul\NairaExchangeRates\Traits\CurrencyTraits;
 use Infinitypaul\NairaExchangeRates\Traits\DateTraits;
 use Infinitypaul\NairaExchangeRates\Traits\TypeTraits;
 
-
 class NairaExchangeRates
 {
     use CurrencyTraits, TypeTraits, DateTraits;
     /**
-     * Naira Exchange Rate API Base Url
+     * Naira Exchange Rate API Base Url.
      */
     const baseURL = 'http://nairaexchangerate.herokuapp.com/api/v1/';
 
-
     /**
-     * Access token
+     * Access token.
      * @var string
      */
     protected $accessToken;
 
     /**
-     *  Response from requests made to Naira Exchange Rate
+     *  Response from requests made to Naira Exchange Rate.
      * @var mixed
      */
     protected $response;
 
-    # The type (default is cbn):
+    // The type (default is cbn):
     private $type;
 
     /**
-     * Instance of Guzzle Client
+     * Instance of Guzzle Client.
      * @var object
      */
     protected $client;
 
-
-    # The base currency (default is USD):
+    // The base currency (default is USD):
     private $baseCurrency;
 
-    # Regular Expression for the currency:
+    // Regular Expression for the currency:
     private $currencyRegExp = '/^[A-Z]{3}$/';
 
-    # Date from which to request historic rates:
+    // Date from which to request historic rates:
     private $dateFrom;
 
-    # Date to which to request historic rates:
+    // Date to which to request historic rates:
     private $dateTo;
 
-    # Supported currencies:
+    // Supported currencies:
     private $_currencies = [
-        'USD', 'GBP', 'EUR', 'JPY', 'XAF', 'CNY', 'QAR', 'ZAR', 'SEK'
+        'USD', 'GBP', 'EUR', 'JPY', 'XAF', 'CNY', 'QAR', 'ZAR', 'SEK',
     ];
 
     private $_types = [
-      'cbn', 'bdc', 'bank', 'moneygram', 'westernunion'
+        'cbn', 'bdc', 'bank', 'moneygram', 'westernunion',
     ];
 
-    # Regular Expression for the date:
+    // Regular Expression for the date:
     private $dateRegExp = '/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/';
 
     public function __construct($accessToken = null)
@@ -74,7 +71,8 @@ class NairaExchangeRates
         $this->prepareRequest();
     }
 
-    private function prepareRequest(){
+    private function prepareRequest()
+    {
         $this->client = new Client(['base_uri' => self::baseURL, 'headers' => ['Authorization' => 'Bearer '.$this->accessToken]]);
     }
 
@@ -83,48 +81,40 @@ class NairaExchangeRates
         return json_decode($this->response->getBody());
     }
 
-    public function performGetRequest($relativeUrl, $params){
-
+    public function performGetRequest($relativeUrl, $params)
+    {
         $this->response = $this->client->request('GET', $relativeUrl, $params);
 
         return $this->getResponse();
     }
 
+    public function fetch()
+    {
+        // Build the URL:
+        $params = [];
 
-    public function fetch(){
-        # Build the URL:
-        $params = [ ];
-
-        # Set the relevant endpoint:
+        // Set the relevant endpoint:
         $endpoint = (is_null($this->dateFrom)) ? '/latest' : '/history';
 
-        # Add dateFrom if specified:
-        if( ! is_null($this->getDateFrom()) )
-        {
+        // Add dateFrom if specified:
+        if (! is_null($this->getDateFrom())) {
             $params['start_at'] = $this->getDateFrom();
         }
 
-        # Add a dateTo:
-        if( ! is_null($this->getDateTo()) )
-        {
+        // Add a dateTo:
+        if (! is_null($this->getDateTo())) {
             $params['end_at'] = $this->getDateTo();
         }
 
-        # Set the base currency:
-        if( ! is_null($this->getBaseCurrency()) )
-        {
+        // Set the base currency:
+        if (! is_null($this->getBaseCurrency())) {
             $params['currency'] = $this->getBaseCurrency();
         }
 
-
-        try{
-           return  $this->performGetRequest($this->getBaseType().$endpoint, [ 'query' => $params ]);
-        }catch (Exception $exception){
-            throw new Exception( $exception->getMessage() );
+        try {
+            return  $this->performGetRequest($this->getBaseType().$endpoint, ['query' => $params]);
+        } catch (Exception $exception) {
+            throw new Exception($exception->getMessage());
         }
-
-
     }
-
-
 }
--- src/Traits/CurrencyTraits.php
+++ src/Traits/CurrencyTraits.php
@@ -6,81 +6,75 @@ use Infinitypaul\NairaExchangeRates\Exceptions\Exceptions;
 
 trait CurrencyTraits
 {
-    # Sanitize a currency code:
-    private function sanitizeCurrencyCode( string $code )
+    // Sanitize a currency code:
+    private function sanitizeCurrencyCode(string $code)
     {
         return trim(
-            strtoupper( $code )
+            strtoupper($code)
         );
     }
 
-    # Set the base currency:
-    public function setBaseCurrency( string $currency )
+    // Set the base currency:
+    public function setBaseCurrency(string $currency)
     {
-        # Sanitize the code:
+        // Sanitize the code:
         $currencyCode = $this->sanitizeCurrencyCode($currency);
 
-        # Is it valid?
-        $this->verifyCurrencyCode( $currencyCode );
+        // Is it valid?
+        $this->verifyCurrencyCode($currencyCode);
 
         $this->baseCurrency = $currencyCode;
 
-        # Return object to preserve method-chaining:
+        // Return object to preserve method-chaining:
         return $this;
     }
 
-
-    # Runs tests to verify a currency code:
-    private function verifyCurrencyCode( string $code )
+    // Runs tests to verify a currency code:
+    private function verifyCurrencyCode(string $code)
     {
         $currencyCode = $this->sanitizeCurrencyCode($code);
 
-        # Is the currency code in ISO 4217 format?
-        if( ! $this->validateCurrencyCodeFormat($currencyCode) )
-        {
+        // Is the currency code in ISO 4217 format?
+        if (! $this->validateCurrencyCodeFormat($currencyCode)) {
             throw Exceptions::create('format.invalid_currency_code');
         }
 
-        # Is it a supported currency?
-        if( ! $this->currencyIsSupported($currencyCode) )
-        {
+        // Is it a supported currency?
+        if (! $this->currencyIsSupported($currencyCode)) {
             throw Exceptions('format.unsupported_currency');
         }
     }
 
-
-    # Validate a currency code is in the correct format:
-    private function validateCurrencyCodeFormat( string $code = null ){
-        if( !is_null($code) )
-        {
-            # Is the string longer than 3 characters?
-            if( strlen($code) != 3 )
-            {
+    // Validate a currency code is in the correct format:
+    private function validateCurrencyCodeFormat(string $code = null)
+    {
+        if (! is_null($code)) {
+            // Is the string longer than 3 characters?
+            if (strlen($code) != 3) {
                 return false;
             }
 
-            # Does it contain non-alphabetical characters?
-            if( ! preg_match( $this->currencyRegExp, $code) )
-            {
+            // Does it contain non-alphabetical characters?
+            if (! preg_match($this->currencyRegExp, $code)) {
                 return false;
             }
 
             return true;
         }
+
         return false;
     }
 
-    # Check if a currency code is in the supported range:
-    public function currencyIsSupported( string $code )
+    // Check if a currency code is in the supported range:
+    public function currencyIsSupported(string $code)
     {
         $currencyCode = $this->sanitizeCurrencyCode($code);
 
-        if( ! $this->validateCurrencyCodeFormat($currencyCode) )
-        {
+        if (! $this->validateCurrencyCodeFormat($currencyCode)) {
             throw Exceptions::create('format.invalid_currency_code');
         }
 
-        return in_array( $currencyCode, $this->_currencies );
+        return in_array($currencyCode, $this->_currencies);
     }
 
     public function getBaseCurrency()
@@ -88,11 +82,9 @@ trait CurrencyTraits
         return (is_null($this->baseCurrency)) ? null : $this->baseCurrency;
     }
 
-    # Get the supported currencies:
-    public function getSupportedCurrencies( string $concat = null )
+    // Get the supported currencies:
+    public function getSupportedCurrencies(string $concat = null)
     {
         return (is_null($concat)) ? $this->_currencies : implode($concat, $this->_currencies);
     }
-
-
 }
